9c470376dc8ef7544f13b4510d6e0136543d06fa
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-22 04:31:49 -0300

    add page home

---
+++ src/pages/home.tsx
@@ -0,0 +1,44 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleImg from '../assets/images/google-logo.svg';
+import loginImg from '../assets/images/log-in.svg';
+import { Button } from '../components/Button'
+
+
+import '../styles/auth.scss'
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração de perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A em tempo real.</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>  
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo LetMeAsk" />
+          <button className="create-room">
+            <img src={googleImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">Ou entre em uma sala</div>
+
+          <form action="">
+            <input 
+              type="text" 
+              placeholder="Digite o código da sala"
+            />
+            
+            <Button type="submit">
+              <img src={loginImg} alt="Log-in ícone" />
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
