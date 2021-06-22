dcc4ca2d92aecd60103f73b2d6115ba6e756c1cc
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-22 17:52:26 -0300

    add page de criação de salas

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,43 @@
+import { Link } from 'react-router-dom';
+
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import { HomeAside } from '../components/HomeAside';
+
+// import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  // const { user } = useAuth();
+
+  return (
+    <div id="page-auth">
+      <HomeAside />
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo LetMeAsk" />
+          <h2>Crie uma nova sala</h2>
+
+          <form action="">
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            
+            <Button type="submit">
+              Criar Sala
+            </Button>
+          </form>
+
+          <p>
+            Quer entrar em uma sala já existente? 
+            <Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
