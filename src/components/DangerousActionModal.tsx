90ca0ca8563a07b9964e54b100e9a09fac57ca8f
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-24 19:43:55 -0300

    update modal de ação perigosa

--- src/components/DangerousActionModal.tsx
+++ src/components/DangerousActionModal.tsx
@@ -1,19 +1,52 @@
-import dangerImg from '../assets/images/danger.svg'
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+import closeRoomIcon from '../assets/images/modal-close.svg'
+import deleteQuestionIcon from '../assets/images/modal-delete.svg'
 
 import { useModal } from '../hooks/useModal'
 
 import '../styles/dangerous-action-modal.scss';
 
-type DangerousActionModalType = {
-  title: string,
-  description: string,
-  buttonText: string,
-  handleModalAction: () => void
+type DangerousActionModalProps = {
+  type: string;
+  handleModalAction: () => void;
 }
 
-export function DangerousActionModal({ title, description, buttonText, handleModalAction }: DangerousActionModalType) {
+type ModalInfoProps = {
+  title: string;
+  description: string;
+  buttonText: string;
+}
 
-  const { isHidden, setIsHidden } = useModal();
+export function DangerousActionModal({ handleModalAction }: DangerousActionModalProps) {
+
+  const { isHidden, setIsHidden, type } = useModal();
+
+  const [modalInfo, setModalInfo] = useState<ModalInfoProps>();
+
+  useEffect(() => {
+    switch (type) {
+      case 'room':
+        setModalInfo({
+          title: 'Encerrar sala',
+          description: 'Tem certeza que você deseja encerrar esta sala?',
+          buttonText: 'Sim, encerrar'
+        });
+        break;
+      
+      case 'question':
+        setModalInfo({
+          title: 'Excluir pergunta',
+          description: 'Tem certeza que você deseja excluir esta pergunta?',
+          buttonText: 'Sim, excluir'
+        });
+        break;
+    
+      default:
+        break;
+    }
+  }, [type])
 
   function toggleModal() {
     setIsHidden(true);
@@ -31,9 +64,9 @@ export function DangerousActionModal({ title, description, buttonText, handleMod
   return (
     <div className="modal-body" style={isHidden ? hide : show}>
       <div className="modal-content">
-        <img src={dangerImg} alt="Imagem de perigo" />
-        <h1>{title}</h1>
-        <p>{description}</p>
+        <img src={type === 'room' ? closeRoomIcon : deleteQuestionIcon} alt="Imagem de perigo" />
+        <h1>{modalInfo?.title}</h1>
+        <p>{modalInfo?.description}</p>
         <div className="modal-buttons">
 
           <button onClick={toggleModal} className="btn-cancel">
@@ -41,7 +74,7 @@ export function DangerousActionModal({ title, description, buttonText, handleMod
           </button>
 
           <button onClick={handleModalAction} className="btn-confirm">
-            {buttonText}
+            {modalInfo?.buttonText}
           </button>
 
         </div>
--- src/contexts/ModalContext.tsx
+++ src/contexts/ModalContext.tsx
@@ -3,8 +3,10 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { createContext } from "react";
 
 type ModalType = {
-  isHidden: boolean,
-  setIsHidden: Dispatch<SetStateAction<boolean>>
+  isHidden: boolean;
+  setIsHidden: Dispatch<SetStateAction<boolean>>;
+  type: string;
+  setType: Dispatch<SetStateAction<string>>;
 }
 
 type PropsType = {
@@ -15,9 +17,10 @@ export const ModalContext = createContext({} as ModalType)
 
 export function ModalContextProvider(props: PropsType) {
   const [isHidden, setIsHidden] = useState(true);
+  const [type, setType] = useState('room');
 
   return (
-    <ModalContext.Provider value={{ isHidden, setIsHidden }}>
+    <ModalContext.Provider value={{ isHidden, setIsHidden, type, setType }}>
       {props.children}
     </ModalContext.Provider>
   );
