6859e8f4b646927277095dcccf4c42176fb3ce34
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-24 04:48:52 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { Home } from "./pages/Home";
+import { Home } from "./pages/home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
 
