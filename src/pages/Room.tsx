07f2e3874488e06cb7d22119d901f01ba6d86a9f
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-24 20:02:12 -0300

    update room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,20 +1,61 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { Header } from '../components/Header'
 import { RoomContent } from '../components/RoomContent'
+import { DangerousActionModal } from '../components/DangerousActionModal';
+
+import { database } from '../services/firebase'
+
+import { useModal } from '../hooks/useModal'
+import { useAuth } from '../hooks/useAuth'
 
 type RoomParams = {
    id: string,
+}
+
+type RoomInfoProps = {
+  authorId: string;
+  title: string;
 }
 
 export function Room(){
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
+  const { type } = useModal();
+  const { user } = useAuth();
+
+  const [roomInfo, setRoomInfo] = useState<RoomInfoProps>({ authorId: '', title: '' });
+
+  useEffect(() => {
+    database.ref(`rooms/${roomId}`).once('value', room => {
+      const firebaseRoom = room.val();
+
+      const values = {
+        authorId: firebaseRoom.authorId,
+        title: firebaseRoom.title,
+      }
+  
+      setRoomInfo(values)
+    })
+  }, [roomId])
+
   return (
     <div id="page-room">
-      <Header code={roomId} />
-      <RoomContent code={roomId} />
+
+      {
+        roomInfo.authorId === user?.id
+          &&
+          <DangerousActionModal
+            type={type}
+            handleModalAction={() => {}}
+          />
+      }
+      
+      <Header code={roomId} authorId={roomInfo.authorId} />
+      <RoomContent code={roomId} authorId={roomInfo.authorId} title={roomInfo.title} />
     </div>
   );
 }
