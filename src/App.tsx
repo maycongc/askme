02ea163e7ba6db3b3d1c88a7c7ce4da7110e9260
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-06-25 01:12:24 -0300

    corrigindo nome do arquivo da pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { Home } from "./pages/home";
+import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
 
