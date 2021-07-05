375c008df3b5f421bb94aede259180db437c7b6f
Maycon Gorgonha <maycon.gorgonha@hotmail.com>
2021-07-05 17:28:04 -0300

    update global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,15 +4,39 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
   }
 
+  html {
+    font-size: 62.5%;
+  }
+
   body {
+    -webkit-font-smoothing: antialiased !important;
     background: ${p => p.theme.colors.background1};
     color: ${p => p.theme.colors.text1};
+
+    @media (max-width: 1080px) {
+      font-size: 1.5rem;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 1.4rem;
+    }
   }
 
   body, input, button, textarea {
-    font: 400 16px 'Roboto', sans-serif;
+    font-weight: 400;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
   }
 `;
